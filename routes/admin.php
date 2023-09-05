bc8ad2ab7c16d689a0c23bfb0310a4ac3641389c
milad <mh3yny@gmail.com>
2023-09-05 17:38:13 +0330

    fix some bugs2

--- routes/admin.php
+++ routes/admin.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('admin')->prefix('')->group(function () {
     Route::get('/', function () {
+		
         return view('admin.index');
     })->name('admin.home');
     Route::resource('/users', UserController::class);
